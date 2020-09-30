9fdb55ae34f8c356060a158cf42c9afeca6e2f3a
ASUS <sonitkach@gmail.com>
2020-09-30 18:03:16 +0300

    use serializers.ModelSerializer

--- book_app/serializers.py
+++ book_app/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Book
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', 'author_name', 'description', 'id')
