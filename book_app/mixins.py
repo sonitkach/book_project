8acd49490a64c56d6afc4194b115877354fcc375
sonitkach <72106899+sonitkach@users.noreply.github.com>
2020-10-10 19:22:39 +0300

    Delete mixins.py

--- book_app/mixins.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.detail import SingleObjectMixin
-
-from .models import Book
-
-class BookDetailMixin(SingleObjectMixin):
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = Book.objects.all()
-        return context
