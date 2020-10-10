934c1a18535e2a105d0d8912c6443b4607cf25ba
sonitkach <72106899+sonitkach@users.noreply.github.com>
2020-10-10 19:21:48 +0300

    #id

--- book_app/models.py
+++ book_app/models.py
@@ -4,7 +4,7 @@ class Book (models.Model):
   title = models.CharField(max_length=100)
   author_name = models.CharField(max_length=100)
   description = models.TextField(blank=True)
-  id = models.AutoField(primary_key=True)
+  #id = models.AutoField(primary_key=True)
 
   def __str__(self):
     return (self.title)
