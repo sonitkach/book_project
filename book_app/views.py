c07bafc8db30eabf96ebd4ce689ef696fea74afe
sonitkach <72106899+sonitkach@users.noreply.github.com>
2020-10-19 18:59:54 +0300

    fix BookViewSet

--- book_app/views.py
+++ book_app/views.py
@@ -1,18 +1,22 @@
 from rest_framework import viewsets, mixins, generics
+from django.views import generic
 
 from book_app.models import Book
-from book_app.serializers import BookSerializer
+from book_app.serializers import BookListSerializer, BookDetailSerializer
 
-class BookViewSet(  viewsets.GenericViewSet,
-                    viewsets.mixins.ListModelMixin,
-                    viewsets.mixins.CreateModelMixin,
-                    viewsets.mixins.RetrieveModelMixin,
-                    viewsets.mixins.UpdateModelMixin,
-                    viewsets.mixins.DestroyModelMixin):
-    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.GenericViewSet,
+                  viewsets.mixins.ListModelMixin,
+                  viewsets.mixins.CreateModelMixin,
+                  viewsets.mixins.RetrieveModelMixin,
+                  viewsets.mixins.UpdateModelMixin,
+                  viewsets.mixins.DestroyModelMixin):
+
+    default_serializer_class = BookListSerializer
     queryset = Book.objects.all()
-    lookup_field = 'id'
+    #lookup_field = 'id'
 
+    '''
     def get(self, request):
         return self.list(request)
 
@@ -24,3 +28,14 @@ class BookViewSet(  viewsets.GenericViewSet,
 
     def delete(self, request):
         return self.destroy(request, id)
+    '''
+
+    serializer_classes = {
+        "list": BookListSerializer,
+        "create": BookDetailSerializer,
+        "retrieve": BookDetailSerializer,
+        "update": BookDetailSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.default_serializer_class)
